2c72eb111a65c893929f9673a9f208ada6f39c99
Tapapriya Shaw <tscode03@gmail.com>
2022-06-15 23:02:41 +0530

    set

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { PostsModule } from './post/posts.module';
+
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ErrorComponent } from './error/error.component';
+
+import { ErrorInterceptor } from './error-interceptor';
+import { AuthInterceptor } from './auth/auth-interceptor';
+
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+     ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AngularMaterialModule,
+    PostsModule,
+
+  ],
+  providers: [
+{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent],
+  // entryComponents: [ErrorComponent]
+})
+export class AppModule {}
---
+++ src/app/post/posts.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+
+import { PostCreateComponent } from './post-create/post-create.component';
+import { PostListComponent } from './post-list/post-list.component';
+
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+
+import { AngularMaterialModule } from "../angular-material.module";
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+declarations: [
+  PostCreateComponent,
+  PostListComponent,
+],
+imports: [
+  CommonModule,
+  ReactiveFormsModule,
+  AngularMaterialModule,
+  RouterModule
+]
+})
+export class PostsModule{}
